1268dc0b0e688ff581d428e1e9a74822567e453b
EnterfaceLLC <dotenterface@gmail.com>
2024-03-26 22:47:42 -0500

    +Amplify _layout config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,10 @@ import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '../amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
